c0233aaf153545a87f22110dc5c9463c7be644c6
skansal <samarthkansal.92@gmail.com>
2018-10-18 02:39:17 -0700

    The comment has been added for the temp bracnh

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,EqualTo,Email
 
+# This is a comment for branch and may be merged with master branch
+
 class RegisterationForm(FlaskForm):
   username = StringField('UserName',validators=[DataRequired(),Length(min=2,max=20)])
   email = StringField('Email',validators=[DataRequired(),Email()])
