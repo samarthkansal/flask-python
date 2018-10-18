764d2a2cef51f8e44a38e9a5c5468141bdd9fdaa
skansal <samarthkansal.92@gmail.com>
2018-10-18 02:26:32 -0700

    Comment has been added

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,EqualTo,Email
 
+# this is comment added for github
+
 class RegisterationForm(FlaskForm):
   username = StringField('UserName',validators=[DataRequired(),Length(min=2,max=20)])
   email = StringField('Email',validators=[DataRequired(),Email()])
