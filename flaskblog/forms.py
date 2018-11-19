26836a86985bc3a0a6fb474faa65bcdf445b19ac
samarthkansal <samarthkansal.92@gmail.com>
2018-11-19 11:01:17 +0530

    Added the account info and option to update it, fixed the previous error but the validator on user and email on Update_Info do not work as expected

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,EqualTo,Email, ValidationError
 from flaskblog.model import User,Post
+from flask_login import current_user
 
 # this is comment added for github
 
@@ -28,20 +29,23 @@ class LoginForm(FlaskForm):
   remember = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
   
-class UpdateInformation():
+class UpdateInformation(FlaskForm):
   username = StringField('UserName',validators=[DataRequired(),Length(min=2,max=20)])
   email = StringField('Email',validators=[DataRequired(),Email()])
   update = SubmitField('Update')
   
   def validate_user(self,username):
-    userr = User.query.filter_by(username = username.data).first()
-    if userr:
-      raise ValidationError(f'This UserName is already taken, Please choose another username')
+    print ("I am in validation of username:::")
+    if current_user.username != username.data:
+      userr = User.query.filter_by(username = username.data).first()
+      if userr:
+        raise ValidationError(f'This UserName is already taken, Please choose another username')
   
   def validate_email(self,email):
-    email = User.query.filter_by(email = email.data).first()
-    if email:
-      raise ValidationError(f'This Email is already registered with us')
+    if current_user.email != email.data:
+      email = User.query.filter_by(email = email.data).first()
+      if email:
+        raise ValidationError(f'This Email is already registered with us, Please User Different One')
 
 
 # This is a comment for branch and may be merged with master branch
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -55,10 +55,20 @@ def logout():
   logout_user()
   return redirect(url_for('login'))
   
-@app.route("/account")
+@app.route("/account",methods = ['GET','POST'])
 @login_required
 def account():
   form = UpdateInformation()
+  
+  if form.validate_on_submit():
+    current_user.username = form.username.data 
+    current_user.email = form.email.data
+    db.session.commit()
+    flash(f'Your Account has been updated !','success')
+    return redirect(url_for('account'))
+  elif request.method == "GET":
+    form.username.data = current_user.username
+    form.email.data = current_user.email
   image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
   return render_template('account.html', title ='Account' ,image_file = image_file, form = form)
 
